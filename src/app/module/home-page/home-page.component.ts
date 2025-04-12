ac4ee84f25eb039761be254a3f81a1107b4879b0
Zail <rekcahet@gmail.com>
2025-04-11 22:35:33 -0500

    fix:  user services

--- src/app/module/home-page/home-page.component.ts
+++ src/app/module/home-page/home-page.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from "../../shared/components/navbar/navbar.component";
 import { SliderComponent } from "./components/slider/slider.component";
+import { UserService } from 'src/app/services/user.service';
+import { HttpClientModule } from '@angular/common/http';
+import { environment } from '@environments/environment.development ';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [NavbarComponent, SliderComponent],
+  imports: [NavbarComponent, SliderComponent,HttpClientModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
 
+
+  constructor(private userService : UserService){
+
+  }
+
+  ngOnInit(): void {
+
+    this.userService.getAllUser().subscribe({
+      next: (data: any) => {
+        console.log('✅ Conexión exitosa. Datos recibidos:', data);
+      },
+      error: (err: any) => {
+        console.error('❌ Error en la conexión:', err);
+      }
+    });
+  }
 }
