50c5a611cbf19444d6884bc06756c7d48799be27
jdnm123 <87243395+jdnm123@users.noreply.github.com>
2025-04-22 18:17:07 -0500

    feat: boton objetos ocultos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoadingComponent } from './shared/components/loading/loading.component';
 import { CarProductComponent } from './shared/components/car-product/car-product.component';
+import { BtnObjectRarityComponent } from './shared/components/btn-object-rarity/btn-object-rarity.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoadingComponent, CarProductComponent],
+  imports: [
+    RouterOutlet,
+    LoadingComponent,
+    CarProductComponent,
+    BtnObjectRarityComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
